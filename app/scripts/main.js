8253d5643dc73b86e95692873ccee5b7e72e9de4
Evgenii Schepotiev <evgenii.schepotiev@jetbrains.com>
2013-04-11 11:24:44 +0200

    ember -> angular

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-window.App = Ember.Application.create({});
+angular.module('users', []).
+    controller("userListController", ["$scope", function ($scope) {
+        $scope.hello = "hi!";
+    }]).
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.
+            when('/users', {templateUrl: 'scripts/templates/users.html', controller: "userListController"}).
+            when('/users/:userId', {templateUrl: 'scripts/template/user.html', controller: "userController"}).
+            otherwise({redirectTo: '/users'});
+    }]);
