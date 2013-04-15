dfc2e769892473f5beba64f2faca66e4b28a89c6
Evgenii Schepotiev <evgenii.schepotiev@jetbrains.com>
2013-04-15 17:50:36 +0200

    fixed name update issue

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,11 +6,12 @@ angular.module('users', ['store']).
         var result = userResource.get({'id': $routeParams.id});
         $scope.user = result;
         $scope.update = function(){
-
             var updatedUser = angular.copy($scope.user);
+            //Workaround over Jersey deserializer if property exists it returns empty list =(
             delete updatedUser.contacts;
             delete updatedUser.groups;
             delete updatedUser.avatar;
+            //end of workaround
             updatedUser.$save();
         }
     }]).
